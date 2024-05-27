f84d2edaa6a308af4782f0782487679a7dde84fd
Heman <daney.heman@teamspirit.com>
2024-05-28 00:07:12 +0800

    exercise 4 and 5

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import styled from 'styled-components/macro';
-
-import { COLORS, WEIGHTS,QUERIES } from '../../constants';
+import { COLORS, WEIGHTS, QUERIES } from '../../constants';
 import Logo from '../Logo';
 import SuperHeader from '../SuperHeader';
 import MobileMenu from '../MobileMenu';
@@ -9,91 +8,89 @@ import UnstyledButton from "../UnstyledButton";
 import Icon from "../Icon";
 
 const Header = () => {
-  const [showMobileMenu, setShowMobileMenu] = React.useState(false);
+    const [showMobileMenu, setShowMobileMenu] = React.useState(false);
+    const handleMobileMenuToggle = () => {
+        console.log('Toggling mobile menu:', !showMobileMenu);
+        setShowMobileMenu(prevState => {
+            console.log('Updating state to:', !prevState);
+            return !prevState;
+        });
+    };
 
-  // For our mobile hamburger menu, we'll want to use a button
-  // with an onClick handler, something like this:
-  //
-  // <button onClick={() => setShowMobileMenu(true)}>
-    const RenderMobileMenuButton = ()=>showMobileMenu ?
-        <UnstyledButton onClick={() => setShowMobileMenu(false)}>
-            <Icon id={'close'} strokeWidth={2} ></Icon>
-        </UnstyledButton>:
-        <UnstyledButton onClick={() => setShowMobileMenu(true)}>
-            <Icon id={'menu'} strokeWidth={2} ></Icon>
-        </UnstyledButton>
-
-  return (
-    <header>
-      <SuperHeader />
-      <MainHeader>
-        <Side>
-          <Logo />
-        </Side>
-        <Nav>
-          <NavLink href="/sale">Sale</NavLink>
-          <NavLink href="/new">New&nbsp;Releases</NavLink>
-          <NavLink href="/men">Men</NavLink>
-          <NavLink href="/women">Women</NavLink>
-          <NavLink href="/kids">Kids</NavLink>
-          <NavLink href="/collections">Collections</NavLink>
-        </Nav>
-         <Side>
-             <MobileIcons>
-                 <UnstyledButton>
-                     <Icon id={'shopping-bag'} strokeWidth={2}></Icon>
-                 </UnstyledButton>
-                 <UnstyledButton>
-                     <Icon id={'search'} strokeWidth={2}></Icon>
-                 </UnstyledButton>
-                 {RenderMobileMenuButton()}
-             </MobileIcons>
-        </Side>
-      </MainHeader>
-
-      <MobileMenu
-        isOpen={showMobileMenu}
-        onDismiss={() => setShowMobileMenu(false)}
-      />
-    </header>
-  );
+    return (
+        <header>
+            <SuperHeader />
+            <MainHeader>
+                <Side>
+                    <Logo />
+                </Side>
+                <Nav>
+                    <NavLink href="/sale">Sale</NavLink>
+                    <NavLink href="/new">New&nbsp;Releases</NavLink>
+                    <NavLink href="/men">Men</NavLink>
+                    <NavLink href="/women">Women</NavLink>
+                    <NavLink href="/kids">Kids</NavLink>
+                    <NavLink href="/collections">Collections</NavLink>
+                </Nav>
+                <Side>
+                    <MobileIcons>
+                        <UnstyledButton>
+                            <Icon id={'shopping-bag'} strokeWidth={2} />
+                        </UnstyledButton>
+                        <UnstyledButton>
+                            <Icon id={'search'} strokeWidth={2} />
+                        </UnstyledButton>
+                        <MobileMenu
+                            isOpen={showMobileMenu}
+                            onChange={setShowMobileMenu}
+                        >
+                            <UnstyledButton >
+                                <Icon id={showMobileMenu ? 'close' : 'menu'} strokeWidth={2} />
+                            </UnstyledButton>
+                        </MobileMenu>
+                    </MobileIcons>
+                </Side>
+            </MainHeader>
+        </header>
+    );
 };
 
 const MainHeader = styled.div`
   display: flex;
   align-items: baseline;
   padding: 18px 32px;
-  height: 72px;
+
+  overflow: auto;
   border-bottom: 1px solid ${COLORS.gray[300]};
-  @media(${QUERIES.tabletAndBelow}){
-    border-top:8px solid ${COLORS.gray[900]};
+  @media (${QUERIES.tabletAndBelow}) {
+    border-top: 8px solid ${COLORS.gray[900]};
     justify-content: space-between;
   }
 `;
 
 const Nav = styled.nav`
   display: flex;
-  gap: 48px;
+  gap: clamp(0.938rem, 5vw - 2rem, 3rem);
   margin: 0px 48px;
-  @media(${QUERIES.tabletAndBelow}){
-    display:none
+  @media (${QUERIES.tabletAndBelow}) {
+    display: none;
   }
 `;
 
 const Side = styled.div`
   flex: 1;
-  @media(${QUERIES.tabletAndBelow}){
-    flex:0;
+  @media (${QUERIES.tabletAndBelow}) {
+    flex: 0;
   }
 `;
 
-const MobileIcons= styled.div`
-  display:none; 
-  @media(${QUERIES.tabletAndBelow}){
-    display:flex;
-    gap:clamp(0.75rem, 5vw - 1.25rem, 2rem);;
+const MobileIcons = styled.div`
+  display: none;
+  @media (${QUERIES.tabletAndBelow}) {
+    display: flex;
+    gap: clamp(0.75rem, 5vw - 1.25rem, 2rem);
   }
-`
+`;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,35 +1,116 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import styled from 'styled-components/macro';
-import { DialogOverlay, DialogContent } from '@reach/dialog';
-
+import { COLORS } from "../../constants";
 import UnstyledButton from '../UnstyledButton';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
+import * as Dialog from '@radix-ui/react-dialog';
+import { Cross2Icon } from '@radix-ui/react-icons';
 
-const MobileMenu = ({ isOpen, onDismiss }) => {
-  if (!isOpen) {
-    return null;
-  }
+const MobileMenu = ({ isOpen, onChange, children }) => {
+    return (
 
-  return (
-    <div>
-      <button onClick={onDismiss}>Dismiss menu</button>
-      <nav>
-        <a href="/sale">Sale</a>
-        <a href="/new">New&nbsp;Releases</a>
-        <a href="/men">Men</a>
-        <a href="/women">Women</a>
-        <a href="/kids">Kids</a>
-        <a href="/collections">Collections</a>
-      </nav>
-      <footer>
-        <a href="/terms">Terms and Conditions</a>
-        <a href="/privacy">Privacy Policy</a>
-        <a href="/contact">Contact Us</a>
-      </footer>
-    </div>
-  );
+
+        <Dialog.Root open={isOpen} onOpenChange={onChange}>
+            <Dialog.Trigger asChild>
+                {children}
+            </Dialog.Trigger>
+            <Dialog.Portal>
+                <StyledDialogOverlay>
+
+                    <StyledDialogContent>
+                        <StyledDialogClose asChild>
+                            <UnstyledButton aria-label="Close">
+                                <Cross2Icon />
+                            </UnstyledButton>
+                        </StyledDialogClose>
+                        <Wrapper>
+                        <Nav>
+                            <a href="/sale">Sale</a>
+                            <a href="/new">New&nbsp;Releases</a>
+                            <a href="/men">Men</a>
+                            <a href="/women">Women</a>
+                            <a href="/kids">Kids</a>
+                            <a href="/collections">Collections</a>
+                        </Nav>
+                        <Footer>
+                            <a href="/terms">Terms and Conditions</a>
+                            <a href="/privacy">Privacy Policy</a>
+                            <a href="/contact">Contact Us</a>
+                        </Footer>
+                        </Wrapper>
+
+                    </StyledDialogContent>
+                </StyledDialogOverlay>
+            </Dialog.Portal>
+        </Dialog.Root>
+
+    );
 };
+
+const Footer = styled.footer`
+  display:flex;
+  gap: 10px;
+`
+
+const Wrapper = styled.div`
+display:flex;
+flex-direction:column; 
+gap:20px;
+  width:fit-content;
+  margin: 0 auto;
+`
+const Nav = styled.nav`
+display:flex;
+  flex-direction:column;
+  align-items: center;
+  & a{
+    text-decoration:none;
+    color: inherit;
+  }
+`
+
+const StyledDialogClose = styled(Dialog.Close)`
+  width:fit-content; 
+  align-self: flex-end;
+`
+const StyledDialogOverlay = styled(Dialog.Overlay)`
+
+  background-color:hsl(0, 0%,0%,0.4);
+  position: fixed;
+  inset: 0;
+  animation: overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1);
+
+  @keyframes overlayShow {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+`;
+
+const StyledDialogContent = styled(Dialog.Content)`
+  display:flex;
+  flex-direction:column;
+  background-color: white;
+  border-radius: 6px;
+  box-shadow: hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 90vw;
+  max-width: 450px;
+  max-height: 85vh;
+  padding: 25px;
+  animation: contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1);
+
+  &:focus {
+    outline: none;
+  }
+`;
 
 export default MobileMenu;
